e292709ebd00134506a7bc4ddd888cedfd03cbd3
nrgx <alibekkaparov@gmail.com>
2024-02-16 19:16:24 +0600

    ponged

--- app/server.go
+++ app/server.go
@@ -14,9 +14,13 @@ func main() {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	_, err = l.Accept()
+	defer l.Close()
+	c, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+	defer c.Close()
+	c.Write([]byte("+PONG\r\n"))
+	fmt.Println("Responded")
 }
