1850241c3857a4cd9b5313df273fa05f492bb2ad
nrgx <alibekkaparov@gmail.com>
2024-02-17 16:52:03 +0600

    multi-client pong

--- app/server.go
+++ app/server.go
@@ -12,37 +12,41 @@ func main() {
 		fmt.Println("error creating tcp server", err.Error())
 		os.Exit(1)
 	}
+
 	defer func() {
 		if err := listener.Close(); err != nil {
 			fmt.Println("error closing tcp server", err.Error())
 		}
 	}()
+
 	for {
-		fmt.Println("running")
 		conn, err := listener.Accept()
 		if err != nil {
-			fmt.Println("error accepting connection", err.Error())
+			fmt.Println("error establishing connection", err.Error())
 			os.Exit(1)
 		}
-		fmt.Println("accpeted")
 		go func(c net.Conn) {
-			fmt.Println("reading")
-			buf := make([]byte, 1024)
-			n, err := c.Read(buf)
-			if err != nil {
-				fmt.Println("error reading from connection", err.Error())
-				os.Exit(1)
-			}
-			// first 8 bytes are header
-			fmt.Println(string(buf[8:n]))
-			buf = []byte("+PONG\r\n")
-			fmt.Println("writing")
+			buf := []byte("+PONG\r\n")
 			if _, err := c.Write(buf); err != nil {
 				fmt.Println("error writing to connection", err.Error())
 				os.Exit(1)
 			}
 			c.Close()
 		}(conn)
-		fmt.Println("finished")
+	}
+}
+
+func handle(c net.Conn) {
+	defer c.Close()
+	buf := make([]byte, 1024)
+	n, err := c.Read(buf)
+	if err != nil {
+		fmt.Println("error reading from connection", err.Error())
+		os.Exit(1)
+	}
+	fmt.Println(buf[8:n])
+	if _, err := c.Write([]byte("+PONG\r\n")); err != nil {
+		fmt.Println("error writing to connection", err.Error())
+		os.Exit(1)
 	}
 }
