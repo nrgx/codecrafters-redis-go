15178a0f2c07f8f3597e6f345e09a78102281490
nrgx <alibekkaparov@gmail.com>
2024-02-18 20:10:54 +0600

    added parsing and echo

--- app/server.go
+++ app/server.go
@@ -1,13 +1,13 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"io"
 	"net"
 	"os"
+	"strings"
 )
-
-var pong = []byte("+PONG\r\n")
 
 func main() {
 	listener, err := net.Listen("tcp", "0.0.0.0:6379")
@@ -17,21 +17,23 @@ func main() {
 	}
 	defer listener.Close()
 	for {
-		fmt.Println("=====accepting=====")
+		// accept blocks loop
 		conn, err := listener.Accept()
 		if err != nil {
 			fmt.Println("error establishing connection", err.Error())
 			continue
 		}
-		fmt.Println("connection established")
-		go handle(conn)
+		// spawn goroutine in background and pass it a connection
+		go process(conn)
 	}
 }
 
-func handle(conn net.Conn) {
+func process(conn net.Conn) {
+	// till connection is live read and write inside a loop
+	// if EOF found break
+	defer conn.Close()
 	buf := make([]byte, 1024)
 	for {
-		fmt.Println("reading")
 		if _, err := conn.Read(buf); err != nil {
 			if err == io.EOF {
 				break
@@ -39,15 +41,47 @@ func handle(conn net.Conn) {
 			fmt.Println("error reading from connection", err.Error())
 			os.Exit(1)
 		}
-		fmt.Println("read")
-		fmt.Println("writing")
-		if _, err := conn.Write(pong); err != nil {
+		out := parse(buf)
+		fmt.Printf("out: %q\n", out)
+		if _, err := conn.Write(out); err != nil {
 			fmt.Println("error writing to connection", err.Error())
 			os.Exit(1)
 		}
-		fmt.Println("wrote")
 	}
+} 
 
-	conn.Close()
-	fmt.Println("======closing======\n")
+func parse(buf []byte) []byte {
+	if len(buf) < 3 {
+		return nil
+	}
+	var start int
+	var commands [][]byte
+	for i := 0; i < len(buf); i++ {
+		if buf[i] == '$' {
+			length := int(buf[i+1] - 48)
+			start = i + 4
+			commands = append(commands, buf[start:start+length])
+		}
+	}
+	fmt.Printf("commands: %q\n", commands)
+	switch strings.ToLower(string(commands[0])) {
+	case "ping":
+		return pong()
+	case "echo":
+		return echo(commands[1:])
+	}
+	return nil
+}
+
+func pong() []byte {
+	return []byte("+PONG\r\n")
+}
+
+func echo(args [][]byte) []byte {
+	buf := bytes.NewBuffer(nil)
+	for _, arg := range args {
+		// write $LEN\r\nARG\r\n
+		buf.Write([]byte(fmt.Sprintf("$%d\r\n%s\r\n", len(arg), string(arg))))
+	}
+	return buf.Bytes()
 }
