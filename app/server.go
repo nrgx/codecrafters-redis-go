9174077cf2ed8d54a8ae06347c0780924c88dba6
nrgx <alibekkaparov@gmail.com>
2024-02-19 15:40:32 +0600

    set with expiry implemented

--- app/server.go
+++ app/server.go
@@ -6,8 +6,119 @@ import (
 	"io"
 	"net"
 	"os"
+	"strconv"
 	"strings"
+	"sync"
+	"time"
 )
+
+type Value[V any] struct {
+	value  V
+	expiry time.Time
+}
+
+func newVal[V any](value V, expKey string, expiry int) Value[V] {
+	v := Value[V]{}
+	v.value = value
+	key := strings.ToUpper(expKey)
+	var ttl time.Duration
+	if key == "" || key == "PX" {
+		ttl = time.Duration(expiry) * time.Millisecond
+	} else if key == "EX" {
+		ttl = time.Duration(expiry) * time.Second
+	} else {
+		ttl = 0
+	}
+	fmt.Println("TTL", ttl)
+	v.expiry = time.Now().Add(ttl)
+	return v
+}
+
+func (v Value[V]) isExpired() bool {
+	return time.Now().After(v.expiry)
+}
+
+type REDIS[K comparable, V any] struct {
+	data map[K]Value[V]
+	mx   *sync.Mutex
+}
+
+func New[K comparable, V any]() *REDIS[K, V] {
+	redis := &REDIS[K, V]{
+		data: make(map[K]Value[V]),
+		mx:   new(sync.Mutex),
+	}
+
+	go func() {
+		for range time.Tick(5 * time.Second) {
+			fmt.Println("tick every 5 sec")
+			redis.mx.Lock()
+			for k, v := range redis.data {
+				if v.isExpired() {
+					fmt.Println("is expired")
+					delete(redis.data, k)
+				}
+			}
+			redis.mx.Unlock()
+		}
+	}()
+
+	return redis
+}
+
+func (r *REDIS[K, V]) get(k K, val string) []byte {
+	if val != "" {
+		return []byte(fmt.Sprintf("$%d\r\n%s\r\n", len(val), val))
+	}
+	r.mx.Lock()
+	defer r.mx.Unlock()
+	v, ok := r.data[k]
+	if !ok {
+		return NIL
+	}
+	if v.isExpired() {
+		delete(r.data, k)
+		valLen, err := getLen(v.value)
+		if err != nil {
+			fmt.Println("error getting value length", err)
+		}
+		return []byte(fmt.Sprintf("$%d\r\n%s\r\n", valLen, v.value))
+	}
+	return NIL
+}
+
+func getLen(v interface{}) (int, error) {
+	switch val := v.(type) {
+	case string:
+		return len(val), nil
+	case []interface{}:
+		return len(val), nil
+	case map[interface{}][]interface{}:
+		return len(val), nil
+	default:
+		return 0, fmt.Errorf("value `%v` of unknown type `%T`", val, val)
+	}
+}
+
+func (r *REDIS[K, V]) set(k K, v Value[V]) []byte {
+	r.mx.Lock()
+	defer r.mx.Unlock()
+	r.data[k] = v
+	return OK
+}
+
+func (r *REDIS[K, V]) pong() []byte {
+	return []byte("+PONG\r\n")
+}
+
+func (r *REDIS[K, V]) echo(args [][]byte) []byte {
+	buf := bytes.NewBuffer(nil)
+	for _, arg := range args {
+		// write $LEN\r\nARG\r\n
+		buf.Write([]byte(fmt.Sprintf("$%d\r\n%s\r\n", len(arg), string(arg))))
+	}
+	return buf.Bytes()
+}
 
 const (
 	PING = "ping"
@@ -17,13 +128,16 @@ const (
 )
 
 var (
-	// Global map.
-	// Should be guarded by mutex or rewrite as sync.Map for concurrent access
-	m = make(map[string]string)
 	// null bulk string RESP encoded
 	NIL = []byte("$-1\r\n")
 	OK  = []byte("+OK\r\n")
 )
+
+var R *REDIS[string, string] = nil
+
+func init() {
+	R = New[string, string]()
+}
 
 func main() {
 	listener, err := net.Listen("tcp", "0.0.0.0:6379")
@@ -82,54 +196,26 @@ func parse(buf []byte) []byte {
 	fmt.Printf("commands: %q\n", commands)
 	switch strings.ToLower(string(commands[0])) {
 	case PING:
-		return pong()
+		return R.pong()
 	case ECHO:
-		return echo(commands[1:])
+		return R.echo(commands[1:])
 	case GET:
-		return get(commands[1:])
+		key := string(commands[1])
+		if len(commands) > 2 {
+			tmp := string(commands[2])
+			return R.get(key, tmp)
+		}
+		return R.get(key, "")
 	case SET:
-		return set(commands[1:])
+		if len(commands) == 5 {
+			ttl, err := strconv.Atoi(string(commands[4]))
+			if err != nil {
+				return NIL
+			}
+			v := newVal[string](string(commands[2]), string(commands[3]), ttl)
+			return R.set(string(commands[1]), v)
+		}
+		return R.set(string(commands[1]), Value[string]{value: string(commands[2])})
 	}
 	return nil
-}
-
-func pong() []byte {
-	return []byte("+PONG\r\n")
-}
-
-func echo(args [][]byte) []byte {
-	buf := bytes.NewBuffer(nil)
-	for _, arg := range args {
-		// write $LEN\r\nARG\r\n
-		buf.Write([]byte(fmt.Sprintf("$%d\r\n%s\r\n", len(arg), string(arg))))
-	}
-	return buf.Bytes()
-}
-
-func get(args [][]byte) []byte {
-	if len(args) == 0 {
-		return NIL
-	}
-	k := string(args[0])
-	var v string
-	if len(args) == 2 {
-		v = string(args[1])
-	} else {
-		val, ok := m[k]
-		if !ok {
-			return NIL
-		}
-		v = val
-	}
-	return []byte(fmt.Sprintf("$%d\r\n%s\r\n", len(v), v))
-}
-
-func set(args [][]byte) []byte {
-	if len(args) != 2 {
-		return NIL
-	}
-	k := string(args[0])
-	v := string(args[1])
-	m[k] = v
-	return OK
 }
