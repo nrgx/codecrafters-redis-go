a89fd1722258216b9dec67117989460a90f64644
nrgx <alibekkaparov@gmail.com>
2024-02-20 13:55:52 +0600

    last edits

--- app/server.go
+++ app/server.go
@@ -6,56 +6,70 @@ import (
 	"io"
 	"net"
 	"os"
+	"slices"
 	"strconv"
 	"strings"
 	"sync"
 	"time"
 )
 
-type Value[V any] struct {
-	value  V
+func respify(value string) []byte {
+	fmt.Println("value before respify", value)
+	s := fmt.Sprintf("$%d\r\n%s\r\n", len(value), value)
+	fmt.Println("before byte conversion", s)
+	out := []byte(s)
+	fmt.Printf("respify %s\n", out)
+	return out
+}
+
+type Value struct {
+	value  string
 	expiry time.Time
 }
 
-func (v Value[V]) Value() string {
-	return fmt.Sprintf("%s", v.value)
-}
-func (v Value[V]) String() string {
-	return fmt.Sprintf("value:{%v} | expiry: {%v}", v.value, time.Since(v.expiry))
-}
-
-func newVal[V any](value V, expKey string, expiry int) Value[V] {
-	v := Value[V]{}
-	v.value = value
-	key := strings.ToLower(expKey)
-	var ttl time.Duration
-	if key == "px" {
-		ttl = time.Duration(expiry) * time.Millisecond
-	} else if key == "ex" {
-		ttl = time.Duration(expiry) * time.Second
+func newValue(args []string) Value {
+	value := args[1]
+	res := Value{value: value}
+	if len(args) == 4 {
+		xKey := strings.ToLower(args[2])
+		expiry, err := strconv.Atoi(args[3])
+		if err != nil {
+			return res
+		}
+		var ttl time.Duration
+		switch xKey {
+		case "px":
+			ttl = time.Duration(expiry) * time.Millisecond
+		case "ex":
+			ttl = time.Duration(expiry) * time.Second
+		}
+		res.expiry = time.Now().Add(ttl)
+		fmt.Println("TTL", ttl)
 	}
-	fmt.Println("TTL", ttl)
-	v.expiry = time.Now().Add(ttl)
-	return v
+	return res
 }
 
-func (v Value[V]) isExpired() bool {
+func (v Value) isExpired() bool {
 	return time.Now().After(v.expiry)
 }
 
-type REDIS[K comparable, V any] struct {
-	data map[K]Value[V]
+func (v Value) String() string {
+	return v.value
+}
+
+type REDIS struct {
+	data map[string]Value
 	mx   *sync.Mutex
 }
 
-func New[K comparable, V any]() *REDIS[K, V] {
-	redis := &REDIS[K, V]{
-		data: make(map[K]Value[V]),
+func New() *REDIS {
+	redis := &REDIS{
+		data: make(map[string]Value),
 		mx:   new(sync.Mutex),
 	}
 
 	go func() {
-		for range time.Tick(1 * time.Second) {
+		for range time.Tick(100 * time.Millisecond) {
 			redis.checkExpiry()
 		}
 	}()
@@ -63,69 +77,50 @@ func New[K comparable, V any]() *REDIS[K, V] {
 	return redis
 }
 
-func (r *REDIS[K, V]) get(k K, val string) []byte {
-	r.mx.Lock()
-	defer r.mx.Unlock()
-	fmt.Println("key:", k, "check:", r.data[k])
-	v, ok := r.data[k]
-	if !ok {
-		fmt.Println("not ok", r.data[k])
-		if val != "" {
-			return []byte(fmt.Sprintf("$%d\r\n%s\r\n", len(val), val))
-		}
-		return NIL
-	}
-	length, err := getLen(v)
-	if err != nil {
-		fmt.Println("error length", err)
-		return NIL
-	}
-	return []byte(fmt.Sprintf("$%d\r\n%s\r\n", length, v.Value()))
-}
-
-func (r *REDIS[K, V]) checkExpiry() {
+func (r *REDIS) checkExpiry() {
 	r.mx.Lock()
 	defer r.mx.Unlock()
 	for k, v := range r.data {
 		if v.isExpired() {
-			fmt.Println("deleting")
 			delete(r.data, k)
 		}
 	}
 }
 
-func getLen(v interface{}) (int, error) {
-	switch val := v.(type) {
-	case string:
-		return len(val), nil
-	case []interface{}:
-		return len(val), nil
-	case map[interface{}][]interface{}:
-		return len(val), nil
-	case Value[string]:
-		return len(val.value), nil
-	default:
-		return 0, fmt.Errorf("value `%v` of unknown type `%T`", val, val)
-	}
-}
-
-func (r *REDIS[K, V]) set(k K, v Value[V]) []byte {
+func (r *REDIS) get(args []string) []byte {
 	r.mx.Lock()
 	defer r.mx.Unlock()
-	r.data[k] = v
-	fmt.Println(r.data)
+	k := args[0]
+	value, ok := r.data[k]
+	if !ok {
+		if len(args) == 2 {
+			return respify(args[1])
+		}
+		return NIL
+	}
+	return respify(value.value)
+}
+
+func (r *REDIS) set(args []string) []byte {
+	if len(args) <= 1 {
+		fmt.Println("given args less than required", args)
+		return NIL
+	}
+	r.mx.Lock()
+	defer r.mx.Unlock()
+	r.data[args[0]] = newValue(args)
 	return OK
 }
 
-func (r *REDIS[K, V]) pong() []byte {
+func (r *REDIS) pong() []byte {
 	return []byte("+PONG\r\n")
 }
 
-func (r *REDIS[K, V]) echo(args [][]byte) []byte {
+func (r *REDIS) echo(args []string) []byte {
 	buf := bytes.NewBuffer(nil)
-	for _, arg := range args {
+	for _, arg := range args[1:] {
 		// write $LEN\r\nARG\r\n
-		buf.Write([]byte(fmt.Sprintf("$%d\r\n%s\r\n", len(arg), string(arg))))
+		buf.Write(respify(arg))
 	}
 	return buf.Bytes()
 }
@@ -138,15 +133,14 @@ const (
 )
 
 var (
-	// null bulk string RESP encoded
 	NIL = []byte("$-1\r\n")
 	OK  = []byte("+OK\r\n")
 )
 
-var R *REDIS[string, string] = nil
+var R *REDIS = nil
 
 func init() {
-	R = New[string, string]()
+	R = New()
 }
 
 func main() {
@@ -181,9 +175,7 @@ func process(conn net.Conn) {
 			fmt.Println("error reading from connection", err.Error())
 			os.Exit(1)
 		}
-		out := parse(buf)
-		fmt.Printf("out: %q\n", out)
-		if _, err := conn.Write(out); err != nil {
+		if _, err := conn.Write(parse(buf)); err != nil {
 			fmt.Println("error writing to connection", err.Error())
 			os.Exit(1)
 		}
@@ -191,41 +183,23 @@ func process(conn net.Conn) {
 }
 
 func parse(buf []byte) []byte {
-	if len(buf) < 3 {
-		return nil
-	}
-	var start int
-	var commands [][]byte
-	for i := 0; i < len(buf); i++ {
-		if buf[i] == '$' {
-			length := int(buf[i+1] - 48)
-			start = i + 4
-			commands = append(commands, buf[start:start+length])
-		}
-	}
-	fmt.Printf("commands: %q\n", commands)
-	switch strings.ToLower(string(commands[0])) {
+	args := strings.Fields(string(buf))
+	args = slices.DeleteFunc[[]string, string](args, func(s string) bool {
+		return strings.HasPrefix(s, "*") || strings.HasPrefix(s, "$")
+	})
+	cmd := args[0]
+	args = args[1 : len(args)-1]
+	fmt.Printf("args:{%s}", args)
+	switch strings.ToLower(cmd) {
 	case PING:
 		return R.pong()
 	case ECHO:
-		return R.echo(commands[1:])
+		return R.echo(args)
 	case GET:
-		key := string(commands[1])
-		if len(commands) > 2 {
-			tmp := string(commands[2])
-			return R.get(key, tmp)
-		}
-		return R.get(key, "")
+		return R.get(args)
 	case SET:
-		if len(commands) == 5 {
-			ttl, err := strconv.Atoi(string(commands[4]))
-			if err != nil {
-				return NIL
-			}
-			v := newVal[string](string(commands[2]), string(commands[3]), ttl)
-			return R.set(string(commands[1]), v)
-		}
-		return R.set(string(commands[1]), Value[string]{value: string(commands[2])})
+		return R.set(args)
+	default:
+		return NIL
 	}
-	return nil
 }
